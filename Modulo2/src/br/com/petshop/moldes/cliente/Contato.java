7d8d17c4185feabf49ff33dc36f37180d9ed0ac4
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-03 02:52:58 -0300

    Adicionando modificacoes Contato

--- Modulo2/src/br/com/petshop/moldes/cliente/Contato.java
+++ Modulo2/src/br/com/petshop/moldes/cliente/Contato.java
@@ -1,17 +1,35 @@
 package br.com.petshop.moldes.cliente;
 
 public class Contato {
+
+    private Integer idContato;
+    private Cliente cliente;
+    private Integer telefone;
     private String descricao;
-    private String telefone;
 
     @Override
     public String toString() {
         return "Contato{" +
-                "descricao='" + descricao + '\'' +
-                ", telefone='" + telefone + '\'' +
-                '}';
+                ", idContato=" + idContato +
+                ", idCliente=" + (cliente != null ? cliente.getId() : null) +
+                ", nomeCliente=" + (cliente != null ? cliente.getNome() : null) +
+                ", telefone=" + telefone +
+                ", descricao=" + descricao +
+                "}";
     }
 
+    public Integer getIdContato() {
+        return idContato;
+    }
+    public void setIdContato(Integer idContato) {
+        this.idContato = idContato;
+    }
+    public Cliente getCliente() {
+        return cliente;
+    }
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
     public String getDescricao() {
         return descricao;
     }
@@ -20,11 +38,11 @@ public class Contato {
         this.descricao = descricao;
     }
 
-    public String getTelefone() {
+    public Integer getTelefone() {
         return telefone;
     }
 
-    public void setTelefone(String telefone) {
+    public void setTelefone(Integer telefone) {
         this.telefone = telefone;
     }
 }
